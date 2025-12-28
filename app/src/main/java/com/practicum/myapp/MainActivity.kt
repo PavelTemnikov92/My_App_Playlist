236e31ebea5342fb8e58d1a13cd328239a9b5024
PavelTemnikov92 <pavel.temnikov@mail.ru>
2025-12-28 13:47:21 +0600

    29.12

--- app/src/main/java/com/practicum/myapp/MainActivity.kt
+++ app/src/main/java/com/practicum/myapp/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.practicum.myapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class
+MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //  IDs
+        val searchButton = findViewById<Button>(R.id.btnSearch)
+        val mediaButton = findViewById<Button>(R.id.btnMedia)
+        val settingsButton = findViewById<Button>(R.id.btnSettings)
+
+        // 1.button (Search)
+        searchButton.setOnClickListener(object : android.view.View.OnClickListener {
+            override fun onClick(v: android.view.View?) {
+                val intent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
+        // 2.button (Media)
+        mediaButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, MediaActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 3.button (Settings)
+        settingsButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/practicum/myapp/SettingsActivity.kt
@@ -0,0 +1,17 @@
+package com.practicum.myapp
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+        backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+}
